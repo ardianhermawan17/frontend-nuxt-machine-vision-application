5622c3f293d9561dae4eb7ef13d9887404249498
Imanuddin.Ardian <imanuddin.ardian@gmail.com>
2022-07-30 09:28:53 +0700

    add correction in mutation andgetters , page limit

--- store/user/getters.js
+++ store/user/getters.js
@@ -2,8 +2,8 @@ export default {
     filter: (state) => {
         const filterState = Object.assign({}, state.filter)
         filterState.total = state.filter.total ? parseInt(state.filter.total) : null
-        filterState.page = state.filter.page ? parseInt(state.filter.page) : 1
-        filterState.limit = state.filter.limit ? state.filter.limit : 10
+        filterState.page = state.filter.page && state.filter.page <= 999 && state.filter.page >= 0 ? parseInt(state.filter.page) : 1
+        filterState.limit = state.filter.limit && state.filter.limit >= 5 && state.filter.limit <= 50 ? state.filter.limit : 20
         filterState.created = state.filter.created ? state.filter.created : 0
         return filterState
     }
